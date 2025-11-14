a9a0e0090d49fe5d8fef67952e380f0e36ca65ed
kzncod-e <kzncode@yahoo.com>
2025-11-14 15:11:04 +0700

    fix

--- next.config.ts
+++ next.config.ts
@@ -3,11 +3,8 @@ import { initOpenNextCloudflareForDev } from "@opennextjs/cloudflare";
 /** @type {import("next").NextConfig} */
 const nextConfig = {
     /* config options here */
-     cloudflare: {
-    wrangler: {
-      remote: false,
-    },
-  },
+    
+  
 };
 
 
